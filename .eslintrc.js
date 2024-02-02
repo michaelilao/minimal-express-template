119904522c828b8cdb3e2bd22d98ba7be4b3df0e
Michael Ilao <michaelilao@live.com>
2024-02-02 15:07:04 -0500

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-console': ['warn', { allow: ['debug', 'error'] }],
+    'func-names': 'off',
+    'no-plusplus': 'off',
+    'no-process-exit': 'off',
+    'class-methods-use-this': 'off',
+    'max-len': ['error', { code: 150 }],
+    'comma-dangle': 0,
+    'object-curly-newline': 'off',
+    'import/prefer-default-export': 'off',
+  },
+};
